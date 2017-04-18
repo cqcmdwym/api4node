dae44fde7c8912a106fd4684975933f07b3316db
chencoo <cookie.chen@autodesk.com>
2017-04-18 18:06:30 +0800

    filter book

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ var port = process.env.PORT || 3000;
 var bookRouter = express.Router();
 bookRouter.route('/Books')
     .get(function(req,res){
-        Book.find(function(err,books){
+        var query = {};
+        if(req.query.genre){
+            query.genre = req.query.genre;
+        }
+
+        Book.find(query,function(err,books){
             if(err){
                 res.status(500).send(err);
             }
