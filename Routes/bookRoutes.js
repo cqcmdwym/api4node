131230825ffd0c7079802f376322682920184523
chencoo <cookie.chen@autodesk.com>
2017-04-19 14:05:02 +0800

    patch book

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -48,8 +48,30 @@ var routes = function (Book) {
             req.book.author = req.body.author;
             req.book.genre = req.body.genre;
             req.book.read = req.body.read;
-            req.book.save();
-            res.json(req.book);
+            req.book.save(function(err){
+                if(err){
+                    res.status(500).send(err);
+                }
+                else{
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function(req, res){
+            if(req.book._id){
+                delete req.body._id;
+            }
+            for(var p in req.body){
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err){
+                if(err){
+                    res.status(500).send(err);
+                }
+                else{
+                    res.json(req.book);
+                }
+            });
         });
         
     return bookRouter;
