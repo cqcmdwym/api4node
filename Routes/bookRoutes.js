619ecb69955133dc2705af01c7cb0fb1f2aef0bc
chencoo <cookie.chen@autodesk.com>
2017-04-19 13:57:15 +0800

    middleware

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -25,31 +25,31 @@ var routes = function (Book) {
             });
         });
 
-    bookRouter.route('/:bookId')
-        .get(function (req, res) {
-            Book.findById(req.params.bookId, function (err, books) {
+    bookRouter.use('/:bookId', function(req, res, next){
+        Book.findById(req.params.bookId, function (err, book) {
                 if (err) {
                     res.status(500).send(err);
                 }
-                else {
-                    res.json(books);
-                }
-            });
-        })
-        .put(function(req, res){
-            Book.findById(req.params.bookId, function(err, book){
-                if(err){
-                    res.status(500).send(err);
+                else if(book){
+                    req.book = book;
+                    next();
                 }
                 else{
-                    book.title = req.body.title;
-                    book.author = req.body.author;
-                    book.genre = req.body.genre;
-                    book.read = req.body.read;
-                    book.save();
-                    res.json(book);
+                    res.status(404).send('no book found');
                 }
             });
+    });
+    bookRouter.route('/:bookId')
+        .get(function (req, res) {
+            res.json(req.book);
+        })
+        .put(function(req, res){
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save();
+            res.json(req.book);
         });
         
     return bookRouter;
